939b674c3b92e29bd37fd6a59f7b44389273fdbe
Abdelilah <abdouajjouqa@gmail.com>
2025-03-16 01:58:34 +0000

    add post's model properties

--- project/app/Models/posts.php
+++ project/app/Models/posts.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class posts extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+    ];
+
+    protected $hidden = [
+        'user_id',
+    ];
 }
