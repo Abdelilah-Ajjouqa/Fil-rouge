94f98b9271d8571e367ec06f1dae33936f0c6663
Abdelilah <abdouajjouqa@gmail.com>
2025-03-16 01:19:27 +0000

    add column needed to user table

--- project/database/migrations/0001_01_01_000000_create_users_table.php
+++ project/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum ('role', ['admin', 'user'])->default('user');
+            $table->boolval('is_active')->default(true);
+            $table->string('avatar')->nullable();
+            $table->string('cover')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
