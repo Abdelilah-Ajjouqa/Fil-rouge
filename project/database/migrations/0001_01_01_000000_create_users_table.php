5d19cae94b15ea30e145d56dab7113850e9f93a1
Abdelilah <abdouajjouqa@gmail.com>
2025-03-16 01:33:29 +0000

    modify is_active column in users's table, and add table posts with all column needed

--- project/database/migrations/0001_01_01_000000_create_users_table.php
+++ project/database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum ('role', ['admin', 'user'])->default('user');
-            $table->boolval('is_active')->default(true);
+            $table->boolean('is_active')->default(true);
             $table->string('avatar')->nullable();
             $table->string('cover')->nullable();
             $table->rememberToken();
--- project/database/migrations/2025_03_16_011846_create_posts_table.php
+++ project/database/migrations/2025_03_16_011846_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description');
+            $table->string('content');
+            $table->boolean('is_public')->default(true);
             $table->timestamps();
         });
     }
