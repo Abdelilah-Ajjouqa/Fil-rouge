69c860808bc4970175aa7d1e0c8997344572728c
Abdelilah <abdouajjouqa@gmail.com>
2025-03-16 18:07:34 +0000

    add routes to auth, posts, user's methods, and comments

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,8 +1,34 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register'])->name("register");
+Route::post('/login', [AuthController::class, 'login'])->name("login");
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Public routes for posts and comments
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/post/{id}', [PostController::class, 'show'])->name('show');
+Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
+
+Route::middleware('auth:sanctum')->group(function(){
+    // user's routes
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::put('/user/{id}', [UserController::class, 'update'])->name('update');
+    Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('destroy');
+
+    // post's routes
+    Route::post('/post', [PostController::class, 'store'])->name('store');
+    Route::put('/post/{id}', [PostController::class, 'update'])->name('update');
+    Route::delete('/post/{id}', [PostController::class, 'destroy'])->name('destroy');
+
+    // comment's routes
+    Route::post('/comment', [CommentController::class, 'store'])->name('store');
+    Route::put('/comment/{id}', [CommentController::class, 'update'])->name('update');
+    Route::delete('/comment/{id}', [CommentController::class, 'destroy'])->name('destroy');
+});
