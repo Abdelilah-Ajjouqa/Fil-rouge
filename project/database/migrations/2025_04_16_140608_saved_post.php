11123792c2e17c81be8fdae266aba878f22f5e34
Abdelilah <abdouajjouqa@gmail.com>
2025-04-17 22:00:27 +0100

    fix error of creating saved_post table

--- project/database/migrations/2025_04_16_140608_saved_post.php
+++ project/database/migrations/2025_04_16_140608_saved_post.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('saved_post', function (Blueprint $table){
+        Schema::create('saved_post', function (Blueprint $table){
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('post_it')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
             $table->timestamps();
 
             // unaccepte dupicate
